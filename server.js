1412039916cea775a64f3f14863ae426f25d1267
mdasifahamed <mdasifahamed89@yahoo.com>
2024-06-27 15:52:00 +0600

    Cors Added

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const contract = require('./contract.js')
 const app = express()
 const port = 9000
 app.use(bodyParser.json())
+app.use(cors())
 
 
 
